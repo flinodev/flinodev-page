550abbe7525799f05803e251dc3d306961469b65
flinodev <franksuriel_7@hotmail.com>
2026-04-02 14:04:16 -0600

    feat: add image optimization guide, enhance SEO metadata, and introduce education and tech stack data

---
+++ src/data/education.ts
@@ -0,0 +1,22 @@
+export const education = [
+  {
+    icon: "thm",
+    href: "https://tryhackme.com/p/flinodev",
+  },
+  {
+    icon: "platzi",
+    href: "https://platzi.com/p/franciscosuriellinocadena/",
+  },
+  {
+    icon: "udemy",
+    href: "https://www.udemy.com/user/francisco-suriel-lino-2/",
+  },
+  {
+    icon: "datacamp",
+    href: "https://www.datacamp.com/portfolio/flinodev",
+  },
+  {
+    icon: "devtalles",
+    href: "https://cursos.devtalles.com/certificates/qtc394twqu",
+  },
+];
