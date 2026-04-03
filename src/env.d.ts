4e30d2c0aa6f74180f96eeefc11ec5e5f59d7cf5
flinodev <franksuriel_7@hotmail.com>
2026-04-03 09:32:56 -0600

    feat: add comments functionality to blog and project entries, integrate Giscus for discussion

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly GISCUS_REPO_ID: string;
+  readonly GISCUS_CATEGORY_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
