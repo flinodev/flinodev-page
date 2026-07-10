d87376cc754e26256799f97381d6cf81de218ab6
Francisco Lino <22673954+flinodev@users.noreply.github.com>
2026-07-09 22:59:41 -0600

    feat: enhance EducationCard component by adding label prop for accessibility and update education data with corresponding labels

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,22 +1,27 @@
 export const education = [
   {
     icon: "thm",
+    label: "Perfil de TryHackMe",
     href: "https://tryhackme.com/p/flinodev",
   },
   {
     icon: "platzi",
+    label: "Perfil de Platzi",
     href: "https://platzi.com/p/franciscosuriellinocadena/",
   },
   {
     icon: "udemy",
+    label: "Perfil de Udemy",
     href: "https://www.udemy.com/user/francisco-suriel-lino-2/",
   },
   {
     icon: "datacamp",
+    label: "Portafolio de DataCamp",
     href: "https://www.datacamp.com/portfolio/flinodev",
   },
   {
     icon: "devtalles",
+    label: "Certificado de DevTalles",
     href: "https://cursos.devtalles.com/certificates/qtc394twqu",
   },
 ];
